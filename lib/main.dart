2f40829d23304787bdf6320ae3dd8eb293c01dbf
y5347M <y5347.morimoto387@gmail.com>
2023-03-11 01:46:35 +0900

    animate3

--- lib/main.dart
+++ lib/main.dart
@@ -36,7 +36,15 @@ class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
     super.initState();
     controller =
         AnimationController(duration: const Duration(seconds: 2), vsync: this);
-    animation = Tween<double>(begin: 0, end: 300).animate(controller);
+    animation = Tween<double>(begin: 0, end: 300).animate(controller)
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          controller.reverse();
+        } else if (status == AnimationStatus.dismissed) {
+          controller.forward();
+        }
+      })
+      ..addStatusListener((status) => print('$status'));
     controller.forward();
   }
 
