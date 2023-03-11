0225643c926896759f4823c71998385ddb50abd5
y5347M <y5347.morimoto387@gmail.com>
2023-03-11 12:12:07 +0900

    animate5

--- lib/main.dart
+++ lib/main.dart
@@ -2,39 +2,26 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(const LogoApp());
 
-class LogoWidget extends StatelessWidget {
-  const LogoWidget({super.key});
+class AnimatedLogo extends AnimatedWidget {
+  const AnimatedLogo({super.key, required Animation<double> animation})
+      : super(listenable: animation);
 
-// Leave out the height and width so it fills the animating parent
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(vertical: 10),
-      child: const FlutterLogo(),
-    );
-  }
-}
-
-class GrowTransition extends StatelessWidget {
-  const GrowTransition(
-      {required this.child, required this.animation, super.key});
-
-  final Widget child;
-  final Animation<double> animation;
+  // Make the Tweens static because they don't change.
+  static final _opacityTween = Tween<double>(begin: 0.1, end: 1);
+  static final _sizeTween = Tween<double>(begin: 0, end: 300);
 
   @override
   Widget build(BuildContext context) {
+    final animation = listenable as Animation<double>;
     return Center(
-      child: AnimatedBuilder(
-        animation: animation,
-        builder: (context, child) {
-          return SizedBox(
-            height: animation.value,
-            width: animation.value,
-            child: child,
-          );
-        },
-        child: child,
+      child: Opacity(
+        opacity: _opacityTween.evaluate(animation),
+        child: Container(
+          margin: const EdgeInsets.symmetric(vertical: 10),
+          height: _sizeTween.evaluate(animation),
+          width: _sizeTween.evaluate(animation),
+          child: const FlutterLogo(),
+        ),
       ),
     );
   }
@@ -56,25 +43,19 @@ class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
     super.initState();
     controller =
         AnimationController(duration: const Duration(seconds: 2), vsync: this);
-    animation = Tween<double>(begin: 0, end: 300).animate(controller)
+    animation = CurvedAnimation(parent: controller, curve: Curves.easeIn)
       ..addStatusListener((status) {
         if (status == AnimationStatus.completed) {
           controller.reverse();
         } else if (status == AnimationStatus.dismissed) {
           controller.forward();
         }
-      })
-      ..addStatusListener((status) => print('$status'));
+      });
     controller.forward();
   }
 
   @override
-  Widget build(BuildContext context) {
-    return GrowTransition(
-      animation: animation,
-      child: const LogoWidget(),
-    );
-  }
+  Widget build(BuildContext context) => AnimatedLogo(animation: animation);
 
   @override
   void dispose() {
